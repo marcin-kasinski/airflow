e4f9fd96985e884ad4c9a6ea1107d3deb0d519e2
marcin <marcin.kasinski@gmail.com>
2019-06-21 21:53:54 +0200

    .

---
+++ dags/custom.py
@@ -0,0 +1,45 @@
+import logging
+
+from airflow.models import BaseOperator
+from airflow.plugins_manager import AirflowPlugin
+from airflow.utils.decorators import apply_defaults
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+#from airflow.operators import MyFirstOperator
+
+log = logging.getLogger(__name__)
+
+class MyFirstOperator(BaseOperator):
+
+    @apply_defaults
+    def __init__(self, my_operator_param, *args, **kwargs):
+        self.operator_param = my_operator_param
+        super(MyFirstOperator, self).__init__(*args, **kwargs)
+
+    def execute(self, context):
+        log.info("Hello WorldXXXXXXXXXXXXXXX!")
+        log.info('operator_param: %s', self.operator_param)
+
+class MyFirstPlugin(AirflowPlugin):
+    name = "my_first_plugin"
+    operators = [MyFirstOperator]
+
+
+dag = DAG('my_test_dag', description='Another tutorial DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
+
+operator_task = MyFirstOperator(my_operator_param='This is a test.',
+                                task_id='my_first_operator_task', dag=dag)
+operator_task2 = MyFirstOperator(my_operator_param='This is a test2.',
+                                task_id='my_first_operator_task2', dag=dag)
+
+operator_task2 = MyFirstOperator(my_operator_param='This is a test3.',
+                                task_id='my_first_operator_task3', dag=dag)
+
+dummy_task >> operator_task
+operator_task >> operator_task2
+operator_task2 >> operator_task3
--- dags/response_rate_etl.py
+++ dags/response_rate_etl.py
@@ -1,15 +1,33 @@
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import MyFirstOperator
+"""
+A DAG definition file in Airflow, written in Python.
+"""
+from datetime import datetime, timedelta
+from airflow.models import DAG  # Import the DAG class
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.sensors import TimeDeltaSensor
 
-dag = DAG('my_test_dag', description='Another tutorial DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+default_args = {
+    'owner': 'you',
+    'depends_on_past': False,
+    'inparam': 'xxxyyyzzz',
+    'start_date': datetime(2018, 1, 8),
+}
 
-dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
+dag = DAG(
+    dag_id='anatomy_of_a_dag3',
+    description="This describes my DAG",
+    default_args=default_args,
+    schedule_interval=timedelta(days=1))   # This is a daily DAG.
 
-operator_task = MyFirstOperator(my_operator_param='This is a test.',
-                                task_id='my_first_operator_task', dag=dag)
+# t0 and t1 are examples of tasks created by instantiating operators
+t0 = TimeDeltaSensor(
+    task_id='wait_a_second',
+    delta=timedelta(seconds=1),
+    dag=dag)
 
-dummy_task >> operator_task
+t1 = BashOperator(
+    task_id='print_date_in_bash',
+    bash_command='date',
+    dag=dag)
+
+t1.set_upstream(t0)
